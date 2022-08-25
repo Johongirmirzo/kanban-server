15a6d7a05e467b79fc9c3cd9d6617fa33b473ae9
Johongirr <johongir_rahimov@bk.ru>
2022-08-25 14:27:47 +0200

    added db

--- config/connectDB.js
+++ config/connectDB.js
@@ -2,9 +2,11 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    mongoose.connect("mongodb://localhost/tkmg").then(() => {
-      console.log("Database connection established!");
-    });
+    mongoose
+      .connect(process.env.MONGO_URI || "mongodb://localhost/tkmg")
+      .then(() => {
+        console.log("Database connection established!");
+      });
   } catch (err) {
     throw new Error(err);
   }
