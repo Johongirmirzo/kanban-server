a9e43a5538983c84667bf52f425b4b69bf29785f
Johongirr <johongir_rahimov@bk.ru>
2022-08-25 16:08:37 +0200

    fixed issue

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const server = new ApolloServer({
   context: ({ req }) => ({ req }),
 });
 
+console.log(process.env.PORT, process.env.MONGODB_URI);
 const PORT = process.env.PORT || 5500;
 server.listen({ port: PORT }).then(({ url }) => {
   connectDB();
