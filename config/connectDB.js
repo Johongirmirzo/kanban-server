2bbd5bf2970a164b98702ad73059e94883facbb3
Johongirr <johongir_rahimov@bk.ru>
2022-08-25 16:03:17 +0200

    fixed issue

--- config/connectDB.js
+++ config/connectDB.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     mongoose
-      .connect(process.env.MONGO_URI || "mongodb://localhost/tkmg")
+      .connect(process.env.MONGODB_URI || "mongodb://localhost/tkmg")
       .then(() => {
         console.log("Database connection established!");
       });
